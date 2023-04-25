ac9ac3da531c8f1baaf26e86fc43170d43b5ed9f
davidmuir1999 <david.bme.muir2017@gmail.com>
2023-04-25 19:05:02 +0100

    Project Upload

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAO2CvfDcWjn8xqMPl4FeG1e90x3kPGUZg",
+  authDomain: "gym-app-creative-project-b93a5.firebaseapp.com",
+  projectId: "gym-app-creative-project-b93a5",
+  storageBucket: "gym-app-creative-project-b93a5.appspot.com",
+  messagingSenderId: "881848605592",
+  appId: "1:881848605592:web:dce42dae22265fd1e1a9ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
